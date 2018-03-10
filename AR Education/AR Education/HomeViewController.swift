79821fac89895cc20388011ad09b3b4cca34d311
Howard Wang <howardwang00@users.noreply.github.com>
2018-03-10 12:53:17 -0800

    Preliminary Home UI, Added UIColor Extension

---
+++ AR Education/AR Education/HomeViewController.swift
@@ -0,0 +1,35 @@
+//
+//  HomeViewController.swift
+//  AR Education
+//
+//  Created by Howard Wang on 3/10/18.
+//  Copyright © 2018 Mango Apps. All rights reserved.
+//
+
+import UIKit
+
+class HomeViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
